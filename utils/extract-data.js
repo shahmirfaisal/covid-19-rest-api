23cb055e3f1859f26223326713ae763fb4862211
Shahmir Faisal <shahmir049@gmail.com>
2020-07-21 22:18:13 +0500

    trimmed text

--- utils/extract-data.js
+++ utils/extract-data.js
@@ -1,13 +1,13 @@
 module.exports = ($, row) => {
   const name = $(row).children("td").eq(1).text();
-  const totalCases = $(row).children("td").eq(2).text() || 0;
-  const newCases = $(row).children("td").eq(3).text() || 0;
-  const totalDeaths = $(row).children("td").eq(4).text() || 0;
-  const newDeaths = $(row).children("td").eq(5).text() || 0;
-  const totalRecovered = $(row).children("td").eq(6).text() || 0;
-  const activeCases = $(row).children("td").eq(8).text() || 0;
-  const criticalCases = $(row).children("td").eq(9).text() || 0;
-  const population = $(row).children("td").eq(14).text() || 0;
+  const totalCases = $(row).children("td").eq(2).text().trim() || 0;
+  const newCases = $(row).children("td").eq(3).text().trim() || 0;
+  const totalDeaths = $(row).children("td").eq(4).text().trim() || 0;
+  const newDeaths = $(row).children("td").eq(5).text().trim() || 0;
+  const totalRecovered = $(row).children("td").eq(6).text().trim() || 0;
+  const activeCases = $(row).children("td").eq(8).text().trim() || 0;
+  const criticalCases = $(row).children("td").eq(9).text().trim() || 0;
+  const population = $(row).children("td").eq(14).text().trim() || 0;
 
   const country = {
     name,
